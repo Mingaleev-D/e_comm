04bac43349de35729eda57b9064102f1de6c616b
Dinar <mingaleev.d92@mail.ru>
2024-12-29 02:36:46 +0300

    navigation

--- app/src/main/java/com/example/e_comm/ui/fragments/AccountOptionsFragment.kt
+++ app/src/main/java/com/example/e_comm/ui/fragments/AccountOptionsFragment.kt
@@ -5,18 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.e_comm.R
+import com.example.e_comm.databinding.FragmentAccountOptionsBinding
+import com.example.e_comm.databinding.FragmentIntrodcutionBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class AccountOptionsFragment : Fragment(R.layout.fragment_account_options) {
+
+    private lateinit var binding: FragmentAccountOptionsBinding
 
     override fun onCreateView(
            inflater: LayoutInflater,
            container: ViewGroup?,
            savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_options, container, false)
+        binding = FragmentAccountOptionsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            btnRegisterAccountOptionsRegister.setOnClickListener {
+                findNavController().navigate(R.id.action_accountOptionsFragment_to_registerFragment)
+            }
+
+            btnRightAddressForShopping.setOnClickListener {
+                findNavController().navigate(R.id.action_accountOptionsFragment_to_loginFragment)
+            }
+        }
     }
 
 
--- app/src/main/java/com/example/e_comm/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/e_comm/ui/fragments/LoginFragment.kt
@@ -1,25 +1,71 @@
 package com.example.e_comm.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.e_comm.R
+import com.example.e_comm.data.utils.Resource
+import com.example.e_comm.databinding.FragmentLoginBinding
+import com.example.e_comm.ui.ShoppingActivity
+import com.example.e_comm.ui.vm.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
-
+    private lateinit var binding: FragmentLoginBinding
+    private val viewModel by viewModels<LoginViewModel>()
 
     override fun onCreateView(
            inflater: LayoutInflater,
            container: ViewGroup?,
            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        return binding.root
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.apply {
+            btnLogin.setOnClickListener {
+                val email = etEmailLogin.text.toString().trim()
+                val password = edPasswordLogin.text.toString()
+
+                viewModel.login(email, password)
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.login.collect {
+                when (it) {
+                    is Resource.Error -> {
+                        Toast.makeText(requireContext(), "${it.message}", Toast.LENGTH_SHORT).show()
+                    }
+
+                    is Resource.Loading -> {
+
+                    }
+
+                    is Resource.Success -> {
+                        Intent(requireActivity(), ShoppingActivity::class.java).also { intent ->
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                        }
+                    }
+
+                    else -> Unit
+                }
+
+            }
+        }
+    }
 }
