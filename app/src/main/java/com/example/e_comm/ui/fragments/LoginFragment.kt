e795a9be7c622251b50e00ffd43e0d3939db1759
Dinar <mingaleev.d92@mail.ru>
2024-12-28 06:22:51 +0300

    start fragment_introdcution.xml

---
+++ app/src/main/java/com/example/e_comm/ui/fragments/LoginFragment.kt
@@ -0,0 +1,25 @@
+package com.example.e_comm.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.e_comm.R
+
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+
+
+    override fun onCreateView(
+           inflater: LayoutInflater,
+           container: ViewGroup?,
+           savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+}
